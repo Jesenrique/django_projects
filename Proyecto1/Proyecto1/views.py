da6396cc794aa038b709a92cc2eee4a511512134
Jesús Enrique García Garnica <jesusenriquegarciag@gmail.com>
2023-09-11 20:38:20 -0500

    uso de loader en los templates

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+from django.template import loader
 
 # crear primera vista
 # cada funcion es una vista, donde se devuelve el texto
@@ -41,14 +42,37 @@ def get_time(request):
 # y en el template se hace referencia a la variable con dos corchetes {{variable}}
 # el nombre de la variable es la key
 def get_time(request):
+    time=datetime.datetime.now()
+
+    # metodo usado sin usar el loader 
+    '''
+    Este forma de cargar el template no se suele usar por el consumo de recursos,
+    en este caso este el codigo:
+    1. Abre un documento, almacena el contenido en doc, usando Template
+    2. Se determina el conjunto de variables a usar en el contexto.
+    3. Se renderiza el contexto.
+
+    en lugar de ellos se usa LOADER el cual se debe importar
     path="./Proyecto1/templates/first_template.html"
     doc=open(path)
     plt=Template(doc.read())
     doc.close()
-    time=datetime.datetime.now()
-    ctx=Context({"actual_time":32})
-
+    ctx=Context({"actual_time":time})
     response=plt.render(ctx)
+    '''
+
+    # Usando el metodo LOADER
+    '''
+    1. importar loader
+    2. en el script settings.py en "DIRS":configurar el path donde se encuentran los 
+        templates.
+    3. obtener el documento usando get_template()
+    4. el contexto sera un diccionario simple, ya no se instacia Context esto para
+        que no ocurran problemas cuando se renderice.
+    '''
+    doc=loader.get_template("first_template.html")
+    ctx={"actual_time":time}
+    response=doc.render(ctx)
 
     return HttpResponse(response)
 
